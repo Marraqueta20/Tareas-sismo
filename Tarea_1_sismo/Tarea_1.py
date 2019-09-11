344870c78ea824d58a34c932b3798bbd1a336f12
Lucas <lucas.sscc@gmail.com>
2019-09-11 01:35:49 -0300

    Inicio de la tarea 1
    aqui me dediqué a buscar los datos y a aprender a leerlos en python

---
+++ Tarea_1_sismo/Tarea_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 00:37:22 2019
+
+@author: Lucas
+"""
+#Tarea 1 sismología
+import matplotlib.pyplot as plt #modulo para graficar
+import numpy as np #este modulo es para trabajar con matrices como en matlab
